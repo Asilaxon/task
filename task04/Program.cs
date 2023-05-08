59dad9535fbcc34fbbaffe775efdd4453adc645d
Asilaxon Aslanova <asilaxon.aslanova@gmail.com>
2023-05-09 01:41:09 +0500

    task 04

---
+++ task04/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.WriteLine("Введите число А");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число D");
+int numD = Convert.ToInt32(Console.ReadLine());
+
+int max = numA; 
+
+// if (numA > max ) max = numA;
+if (numB > max ) max = numB;
+if (numD > max ) max = numD;
+
+Console.WriteLine($"Максимальное чисоло {max}");
+
+
